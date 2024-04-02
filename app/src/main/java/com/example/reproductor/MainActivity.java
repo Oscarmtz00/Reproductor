34d87dfaa4456283132a1132e54c70f1d120d5a0
Oscar Martinez <diomedesmdiaz@gmail.com>
2024-04-02 16:38:00 -0600

    Avances en las funciones y agregando botones

--- app/src/main/java/com/example/reproductor/MainActivity.java
+++ app/src/main/java/com/example/reproductor/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.reproductor;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,8 +27,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(SavedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_playpausa = (Button) findViewById(R.id.btn_play_pausa);
+        btn_playpausa= (Button) findViewById(R.id.btn_play_pausa);
         btn_repetir = (Button) findViewById(R.id.btn_repetir);
+        iv= (ImageView) findViewById(R.id.imageView);
 
+        vectormp[0]=MediaPlayer.create(this, R.raw.race);
+        vectormp[1]=MediaPlayer.create(this, R.raw.sound);
+        vectormp[2]=MediaPlayer.create(this, R.raw.tea);
+    }
+
+    //Metodo para el boton Play
+
+    public void Play (View view){
+        if (vectormp [posicion].isPlaying()){
+            vectormp [posicion].pause();
+
+            btn_playpausa.setBackgroundResource(R.drawable.reproducir);
+            Toast.makeText(this, "Pausa", Toast.LENGTH_SHORT).show();
+        }else {
+            vectormp[posicion].start();
+            btn_playpausa.setBackgroundResource(R.drawable.pausa);
+            Toast.makeText(this, "Play", Toast.LENGHT_SHORT).show();
+        }
     }
 }
