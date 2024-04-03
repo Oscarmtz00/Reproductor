9c68c51f2c576bee6b3f8af75ff7cc55fafec967
Oscar Martinez <diomedesmdiaz@gmail.com>
2024-04-03 15:36:31 -0600

    Proyecto terminado, conserva la funcion Anterior segun el ejemplo en clase.

--- app/src/main/java/com/example/reproductor/MainActivity.java
+++ app/src/main/java/com/example/reproductor/MainActivity.java
@@ -6,16 +6,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_playpausa, btn_repetir;
+    Button btn_playpausa, btn_repetir, btn_stop, btn_anterior, btn_siguiente;
     MediaPlayer mp;
     ImageView iv;
     int repetir, posicion=0;
@@ -24,11 +19,25 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(SavedInstanceState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_playpausa= (Button) findViewById(R.id.btn_play_pausa);
+        btn_playpausa = (Button) findViewById(R.id.btn_play_pausa);
+        btn_playpausa.setOnClickListener(this::Play);
+
         btn_repetir = (Button) findViewById(R.id.btn_repetir);
+        btn_repetir.setOnClickListener(this::Repetir);
+
+        btn_stop = (Button) findViewById(R.id.btn_stop);
+        btn_stop.setOnClickListener(this::Stop);
+
+        btn_anterior = (Button) findViewById(R.id.btn_anterior);
+        btn_anterior.setOnClickListener(this::Anterior);
+
+        btn_siguiente = (Button) findViewById(R.id.btn_siguiente);
+        btn_siguiente.setOnClickListener(this::Siguiente);
+
+
         iv= (ImageView) findViewById(R.id.imageView);
 
         vectormp[0]=MediaPlayer.create(this, R.raw.race);
@@ -39,15 +48,99 @@ public class MainActivity extends AppCompatActivity {
     //Metodo para el boton Play
 
     public void Play (View view){
+
         if (vectormp [posicion].isPlaying()){
             vectormp [posicion].pause();
-
             btn_playpausa.setBackgroundResource(R.drawable.reproducir);
             Toast.makeText(this, "Pausa", Toast.LENGTH_SHORT).show();
         }else {
             vectormp[posicion].start();
             btn_playpausa.setBackgroundResource(R.drawable.pausa);
-            Toast.makeText(this, "Play", Toast.LENGHT_SHORT).show();
+            Toast.makeText(this, "Play", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //Metodo para el boton Stop
+
+    public void Stop(View view) {
+
+        if (vectormp[posicion]!=null){
+            vectormp[posicion].stop();
+
+            vectormp[0]=MediaPlayer.create(this, R.raw.race);
+            vectormp[1]=MediaPlayer.create(this, R.raw.sound);
+            vectormp[2]=MediaPlayer.create(this, R.raw.tea);
+            posicion=0;
+            btn_playpausa.setBackgroundResource(R.drawable.reproducir);
+            iv.setImageResource(R.drawable.portada1);
+            Toast.makeText(this, "Stop", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //Metodo para el boton Repetir
+
+    public void Repetir(View view){
+
+        if (repetir==1){
+            btn_repetir.setBackgroundResource(R.drawable.no_repetir);
+            Toast.makeText(this, "No repetir", Toast.LENGTH_SHORT).show();
+            vectormp[posicion].setLooping(false);
+            repetir=2;
+        }else {
+            btn_repetir.setBackgroundResource(R.drawable.repetir);
+            Toast.makeText(this, "Repetir", Toast.LENGTH_SHORT).show();
+            vectormp[posicion].setLooping(true);
+            repetir=1;
+        }
+    }
+
+    //Metodo para el boton Siguiente
+
+    public void Siguiente(View view){
+
+        if(posicion< vectormp.length -1){
+            if (vectormp[posicion].isPlaying()){
+                vectormp[posicion].stop();
+                posicion++;
+                vectormp[posicion].start();
+
+                if (posicion==0){
+                    iv.setImageResource(R.drawable.portada1);
+                }else if(posicion==1){
+                    iv.setImageResource(R.drawable.portada2);
+                }else if (posicion==2){
+                    iv.setImageResource(R.drawable.portada3);
+                }
+            }else {
+                posicion++;
+
+                if (posicion==1){
+                    iv.setImageResource(R.drawable.portada1);
+                }else if(posicion==2){
+                    iv.setImageResource(R.drawable.portada2);
+                }else if(posicion==3){
+                    iv.setImageResource(R.drawable.portada3);
+                }
+            }
+        }else {
+            Toast.makeText(this, "No se encuentran mas canciones para reproducir", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //Metodo para el boton anterior
+
+    public void Anterior(View view){
+
+        if(posicion>=1){
+            if (vectormp [posicion].isPlaying()){
+                vectormp[posicion].stop();
+                vectormp[0]=MediaPlayer.create(this, R.raw.race);
+                vectormp[1]=MediaPlayer.create(this, R.raw.sound);
+                vectormp[2]=MediaPlayer.create(this, R.raw.tea);
+                posicion--;
+            }
+        }else {
+            Toast.makeText(this, "No existen mas canciones para reproducir", Toast.LENGTH_SHORT).show();
         }
     }
 }
